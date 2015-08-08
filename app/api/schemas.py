efc809d25dd73b0898f642de3afebd582609066c
Dat Phan <datphan0110@gmail.com>
2015-08-08 12:36:19 +0700

    PILOT-80 | Implement fields_to_dict

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,15 +1,46 @@
 from marshmallow import Schema as SchemaOrigin
-
+import re
 
 def fields_to_dict(fields):
+    """ FIXME:
+        https://www.debuggex.com/r/24QPqzm5EsR0e2bt
+        https://www.debuggex.com/r/0SjmBL55ySna0kFF
+        https://www.debuggex.com/r/Vh9qvHkCV4ZquS14
+    """
     result = {}
 
     if not fields or len(fields.strip()) == 0:
         return result
 
-    for key in fields.split(','):
+    look_behind_keys = re.findall('{(\w*?),', fields)
+    look_behind_pattern_list = ['(?<!{' + k + ')' for k in look_behind_keys]
+
+    # FIXME: '(?<!{[^,]*),<look_forward_pattern>' will trigger "look-behind requires fixed-width pattern"
+    look_behind_pattern = ''.join(look_behind_pattern_list)
+
+    # FIXME: not support nested bracket: field{id,name,description{abc,def}}
+    look_forward_pattern = '(?![a-zA-Z0-9,\}:\[\]]*?})'
+
+    # sample pattern: '(?<!{id)(?<!{email),<look_forward_pattern>'
+    re_pattern = look_behind_pattern + ',' + look_forward_pattern
+
+    for key in re.split(re_pattern, fields):
         key = key.strip()
-        result[key] = {}
+        value = {}
+        if key.find('{') > -1:
+            # get sub fields: field{<sub_fields>} and assign its value
+            sub_field = re.findall('{(.*)}', key)
+            value = fields_to_dict(sub_field[0])
+            # clean key
+            key = re.sub('{(.*)}', '', key)
+
+        if key.find('[') > -1:
+            # get & set slide range: [a:b]
+            value['__slice'] = re.findall('\[(.*)\]', key)[0]
+            # clean key
+            key = re.sub('\[(.*)\]', '', key)
+
+        result[key] = value
 
     return result
 
